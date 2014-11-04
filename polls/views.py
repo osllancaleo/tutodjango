0ed39e58d3afaa52a4111871e5e1948d98cd9695
unknown <3045534@DCL-3746655.cl.lan.com>
2014-11-04 11:33:47 -0300

    Tutorial ok parte 4

--- polls/views.py
+++ polls/views.py
@@ -3,48 +3,39 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 #from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
+from django.views import generic #Para terceras vistas.
 from polls.models import Question, Choice
-# Create your views here.
 
-#def index(request):
-#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#	template = loader.get_template('polls/index.html')
-#	context = RequestContext(request, 
-#		{'latest_question_list': latest_question_list,
-#		})
-#	return HttpResponse(template.render(context))
 
-def index(request):
-	latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-#def detail(request, question_id):
-#	return HttpResponse("Estas viendo la pregunta %s" % question_id)
+	def get_queryset(self):
+		#Retorna las últimas 5 preguntas publicadas
+		return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request,'polls/detail.html',{'question': question})
+class DetailView(generic.DetailView):
+	template_name = 'polls/detail.html'
+	model= Question
 
-def results(request,question_id):
-	response = "Estas viendo los resultados de la encuesta %s"
-	return HttpResponse(response % question_id)
-
-#def vote(request,question_id):
-#	return	HttpResponse("Tu vas a votar en la pregunta %s" % question_id)
+class ResultsView(generic.DetailView):
+	template_name = 'polls/results.html'
+	model = Question	
 
 def vote(request, question_id):
 	p = get_object_or_404(Question, pk=question_id)
 	try:
+		#Busca si se seleccionó alguna opción y se autoenvió el formulario.
 		selected_choice = p.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
+		#Entra por default en en este render
 		return render(request,'polls/detail.html',{
 			'question':p,
-			'error_message': "Debe elegir una de las opciones",
+			'error_message': "Debe elegir una de las opciones...",
 			})
 	else:
+		#Si se seleccionó algo, ingresa a esta opción
 		selected_choice.votes +=1
 		selected_choice.save()
 		return	HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
-
-
